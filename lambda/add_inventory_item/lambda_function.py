c432d6f5a078f0cecd6819972e9cabeb97f5bcff
pinyirao <prao2@rrc.ca>
2025-04-15 12:48:37 -0500

    feat: finish the lambda function development

---
+++ lambda/add_inventory_item/lambda_function.py
@@ -0,0 +1,48 @@
+import json
+import boto3
+import uuid
+import os
+from ulid import ULID
+
+def lambda_handler(event, context):
+    # Parse incoming JSON data
+    try:
+        data = json.loads(event['body'])
+    except KeyError:
+        return {
+            'statusCode': 400,
+            'body': json.dumps("Bad request. Please provide the data.")
+        }
+
+    # Get the table name from environment variable, otherwise use the default table name
+    table_name = os.getenv('TABLE_NAME', 'Inventory')
+
+    # DynamoDB setup
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(table_name)
+
+    # Generate a unique ID using the ulid module
+    unique_id = str(ULID())
+
+    # Insert data into DynamoDB
+    try:
+        table.put_item(
+            Item={
+                '_id': unique_id,
+                'name': data['name'],
+                'description': data['description'],
+                'qty': data['qty'],
+                'price': data['price'],
+                'location_id': data['location_id'],
+
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps(f"Item {data['name']} with ID {unique_id} added successfully.")
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(f"Error adding item: {str(e)}")
+        }
